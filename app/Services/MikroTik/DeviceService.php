9b510ba8cfbd6eff9d5b250624404877305bc868
fie70s <fies6000@gmail.com>
2026-07-16 12:33:28 +0300

    Refactor DeviceService to use RouterOSService and encrypt credentials

--- app/Services/MikroTik/DeviceService.php
+++ app/Services/MikroTik/DeviceService.php
@@ -4,42 +4,33 @@ namespace App\Services\MikroTik;
 
 use App\Models\MikroTikDevice;
 use Exception;
-use RouterOS\Client;
-use RouterOS\Config;
+use Illuminate\Support\Facades\Crypt;
 
 class DeviceService
 {
+    public function __construct(
+        protected RouterOSService $routerOSService
+    ) {
+    }
+
     public function registerDevice(array $data): MikroTikDevice
     {
-        if (!$this->testConnection($data)) {
+        if (!$this->routerOSService->isConnected($data)) {
             throw new Exception('Unable to connect to MikroTik device.');
         }
+
+        $system = $this->routerOSService->getSystemIdentity($data);
 
         return MikroTikDevice::create([
             'name' => $data['title'],
             'ip_address' => $data['ip_address'],
             'api_port' => $data['port'] ?? 8728,
             'username' => $data['username'],
-            'password' => $data['password'],
+            'password' => Crypt::encryptString($data['password']),
+            'router_model' => $system['board-name'] ?? null,
+            'router_os' => $system['version'] ?? null,
             'status' => 'online',
             'last_seen' => now(),
         ]);
-    }
-
-    public function testConnection(array $data): bool
-    {
-        try {
-            $config = new Config();
-            $config->set('host', $data['ip_address']);
-            $config->set('user', $data['username']);
-            $config->set('pass', $data['password']);
-            $config->set('port', $data['port'] ?? 8728);
-
-            new Client($config);
-
-            return true;
-        } catch (Exception $e) {
-            return false;
-        }
     }
 }
